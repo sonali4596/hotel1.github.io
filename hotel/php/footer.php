fad1dbc9418eafddd899673d37a67df2263cf719
sonali4596 <28987021+sonali4596@users.noreply.github.com>
2019-07-04 18:08:09 +0530

    uploading the hotel management website

---
+++ hotel/php/footer.php
@@ -0,0 +1,46 @@
+<div id="footer"  style="border-bottom:4px solid black;">
+ <div class="footer_main_div">
+                <table class="foot_table">
+                        <tr>
+                            <td class="foot_th_mail" >REGISTERED OFFICE</td>
+                            <td class="foot_th_contact">CONTACT US</td>
+                            <td class="foot_th_empty">
+                                
+								<a class="Aa62720181228Br_" href="#"><i class="fa fa-facebook-official" ></i></a>
+                                <a class='Aa62720181228Br_' href='#'><i class="fa fa-google-plus"></i></a>
+                                <a class='Aa62720181228Br_' href='#'><i class="fa fa-linkedin-square"></i></a>
+                                <a class='Aa62720181228Br_' href='#'><i class="fa fa-instagram"></i></a>
+                                <a class='Aa62720181228Br_' href='#'><i class="fa fa-linkedin-square"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="tfoot_th_mail">girls&nbsp;&nbsp; hostal &nbsp;padmanabhanagar&nbsp;&nbsp; Banglore&nbsp;</td>
+                            <td class="tfoot_th_contact">Phone No. : 2548648451</td>
+                            <td class="tfoot_th_empty"><a href="" class="foot_a">Join Our Compnay</a></td>
+                        </tr>
+                        <tr>
+                            <td class="tfoot_th_mail">karnataka&nbsp;&nbsp;&nbsp; bangalore</td>
+                            <td class="tfoot_th_contact">Land Line No. : 0887 22433</td>
+                            <td class="tfoot_th_empty"><a href="" class="foot_a">Our Services</a></td>
+                        </tr>
+                        <tr>
+                            <td class="tfoot_th_mail">Postal Code:&nbsp;&nbsp; 560070</td>
+                            <td class="tfoot_th_contact">Fax No. &nbsp;&nbsp;0532 23874</td>
+                            <td class="tfoot_th_empty"><a href="" class="foot_a">Give Feedback</a></td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" class="foot_payment"><span class="pav"> PAYMENT METHOD ::</span>
+                               <span class="img_pav">
+                                <img src="images/visa-mastercard-amex.png" height="30px" >
+                                <img src="images/Discover.png" height="30px" >
+                                <img src="images/rupay.jpg" height="30px" >
+                                <img src="images/Maestrocard.png" height="40" />
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="foot_copyright" colspan="3">copyright <span>&copy;</span> All Right Reserved from 2010-<?php echo date("Y"); ?>  sonali sinha</td>
+                        </tr>
+                </table>
+            </div>
+       </div>
